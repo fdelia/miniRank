571e7f5fe59b17825f889d9a3810eb5feb8712d8
fdelia <fabio_d@gmx.net>
2018-02-04 19:59:22 +0100

    initial

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "miniRank"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+libraryDependencies += "com.typesafe.slick" %% "slick" % "3.2.1"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+
+import scala.slick.session.Database
+import scala.slick.jdbc.StaticQuery
+import scala.slick.jdbc.meta.MTable
+
+
+object Main extends App {
+  def main(){
+    println("test")
+  }
+  main()
+}
